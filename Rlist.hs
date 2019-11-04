e736ddb4fb7498046deeb1327377ff6651b723b2
Ivy Rogatko <ipr210lehigh@gmail.com>
2019-11-03 17:12:26 -0800

    cons, empty, indexTree

--- Rlist.hs
+++ Rlist.hs
@@ -22,3 +22,22 @@ mapNode f (MakeNode i t) = MakeNode i (mapTree f t)
 
 mapRlist :: (a -> b) -> Rlist a -> Rlist b
 mapRlist f (MakeRlist ns) = MakeRlist (map (mapNode f) ns)
+
+cons :: a -> Rlist a -> Rlist a
+cons item (MakeRlist (MakeNode n t0 : MakeNode m t1 : rest))
+  | n == m = MakeRlist $ MakeNode (n * 2 + 1) (Parent item t0 t1) : rest
+cons item (MakeRlist rlist) = MakeRlist $ MakeNode 1 (Leaf item) : rlist
+
+empty :: Rlist a
+empty = MakeRlist []
+
+indexTree :: Int -> Int -> Tree a -> a
+indexTree index size (Leaf item)
+  | index == 0 = item
+indexTree index size (Parent item left right)
+  | index == 0 = item
+  | otherwise =
+    let subtree_size = div (size - 1) 2
+    in if index <= subtree_size
+       then indexTree (index - 1) subtree_size left
+       else indexTree (index - subtree_size - 1) subtree_size right
