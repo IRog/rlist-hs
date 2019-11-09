55270f9ec3778f5ee12149d7c25eb6b2c73d3124
Ivy Rogatko <ipr210lehigh@gmail.com>
2019-11-09 15:15:57 -0800

    size, tl, hd, uncons

--- Rlist.hs
+++ Rlist.hs
@@ -28,22 +28,53 @@ cons item (MakeRlist (MakeNode n t0 : MakeNode m t1 : rest))
   | n == m = MakeRlist $ MakeNode (n * 2 + 1) (Parent item t0 t1) : rest
 cons item (MakeRlist rlist) = MakeRlist $ MakeNode 1 (Leaf item) : rlist
 
+uncons :: Rlist a -> Maybe (a, Rlist a)
+uncons (MakeRlist []) = Nothing
+uncons (MakeRlist (MakeNode sz (Leaf item) : rest))
+  | sz == 1 = Just (item, MakeRlist rest)
+  | otherwise = error "uncons: leaf size invariant violated"
+uncons (MakeRlist (MakeNode sz (Parent item l r) : rest)) =
+  let new_size = div (sz - 1) 2
+      new_list = MakeRlist $ MakeNode new_size l : MakeNode new_size r : rest
+  in Just (item, new_list)
+
+hd :: Rlist a -> Maybe a
+hd (MakeRlist []) = Nothing
+hd (MakeRlist (MakeNode sz (Leaf item) : _tl))
+  | sz == 1 = Just item
+  | otherwise = error "hd: leaf size invariant violated"
+hd (MakeRlist (MakeNode _size (Parent item _l _r) : _tl)) = Just item
+
+tl :: Rlist a -> Rlist a
+tl (MakeRlist []) = empty
+tl (MakeRlist (MakeNode sz (Leaf _item) : rest))
+  | sz == 1 = MakeRlist rest
+  | otherwise = error "tl: leaf size invariant violated"
+tl (MakeRlist (MakeNode _size (Parent _item _l _r) : rest)) = MakeRlist rest
+
 empty :: Rlist a
 empty = MakeRlist []
 
+size :: Rlist a -> Int
+size = go 0 where
+  go acc (MakeRlist []) = acc
+  go acc (MakeRlist ((MakeNode sz _tree) : rest)) =
+    go (acc + sz) (MakeRlist rest)
+
 indexTree :: Int -> Int -> Tree a -> a
-indexTree index size (Leaf item)
-  | index == 0 = item
-indexTree index size (Parent item left right)
-  | index == 0 = item
+indexTree indx _size (Leaf item)
+  | indx == 0 = item
+  | otherwise = error "indexTree: leaf size invariant violated"
+indexTree indx sz (Parent item left right)
+  | indx == 0 = item
   | otherwise =
-    let subtree_size = div (size - 1) 2
-    in if index <= subtree_size
-       then indexTree (index - 1) subtree_size left
-       else indexTree (index - subtree_size - 1) subtree_size right
+    let subtree_size = div (sz - 1) 2
+    in if indx <= subtree_size
+       then indexTree (indx - 1) subtree_size left
+       else indexTree (indx - subtree_size - 1) subtree_size right
 
 index :: Int -> Rlist a -> Maybe a
-index idx (MakeRlist []) = Nothing
-index idx (MakeRlist ((MakeNode size tree) : rest))
-  | idx < size = Just $ indexTree idx size tree
-  | otherwise = index (idx - size) (MakeRlist rest)
+index _idx (MakeRlist []) = Nothing
+index idx (MakeRlist ((MakeNode sz tree) : rest))
+  | idx < sz = Just $ indexTree idx sz tree
+  | otherwise = index (idx - sz) (MakeRlist rest)
